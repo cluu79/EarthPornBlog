4e9f261352c4c2abc8094b470a5cd7c4ff59c6fe
Danny <cluu79@gmail.com>
2017-06-21 16:11:26 -0500

    added new post page

--- app.js
+++ app.js
@@ -47,6 +47,22 @@ app.get("/blogs", function(req, res){
 	
 });
 
+//create route
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+});
+
+app.post("/blogs", function(req,res){
+	// create blog
+	EarthBlog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			res.render("new");
+		}else{
+			res.redirect("/blogs");
+		}
+	});
+});
+
 
 
 
